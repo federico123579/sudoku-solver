03683fd888c9dac9ccfcb693bf5da3192316407c
Federico Lolli <federico123579@gmail.com>
2022-04-15 23:55:38 +0200

    Added print complete function

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ impl Board {
         Board { board }
     }
 
+    // print utility functions
+
     fn print_simple(&self) {
         for i in 0..9 {
             for j in 0..9 {
@@ -18,9 +20,37 @@ impl Board {
             println!();
         }
     }
+
+    fn print_complete(&self) {
+        for i in 0..10 {
+            match i {
+                0 => println!("┌───────┬───────┬───────┐"),
+                3 | 6 => println!("├───────┼───────┼───────┤"),
+                // last line
+                9 => {
+                    println!("└───────┴───────┴───────┘");
+                    break;
+                }
+                _ => (),
+            }
+            for j in 0..10 {
+                match j {
+                    0 | 3 | 6 => print!("│ "),
+                    // last column
+                    9 => {
+                        print!("│");
+                        break;
+                    }
+                    _ => (),
+                }
+                print!("{} ", self.board[(i, j)])
+            }
+            println!();
+        }
+    }
 }
 
 fn main() {
     let board = Board::init();
-    board.print_simple();
+    board.print_complete();
 }
