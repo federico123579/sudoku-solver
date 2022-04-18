237b00167fb70323ad47b665a759bace324ab98e
Federico Lolli <federico123579@gmail.com>
2022-04-18 11:18:15 +0200

    Fixed print of zeros like empty cell

--- tests/test.rs
+++ tests/test.rs
@@ -1,7 +1,13 @@
 use sudoku::Board;
 
 #[test]
-fn test_init() {
+fn test_print_complete() {
     let board = Board::init();
     board.print_complete();
+}
+
+#[test]
+fn test_print_simple() {
+    let board = Board::init();
+    board.print_simple();
 }
